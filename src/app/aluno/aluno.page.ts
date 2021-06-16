4dba49a4d9f82527904296c1ccc89ae8ce2f2ad2
Cassio <cassioate@gmail.com>
2021-06-16 00:31:37 -0300

    Apenas resolvendo o mesmo problema com o foreach a titulo de conhecimento

--- src/app/aluno/aluno.page.ts
+++ src/app/aluno/aluno.page.ts
@@ -26,17 +26,40 @@ export class AlunoPage implements OnInit {
 
   carregarAlunos(){
     this.alunos = [];
+    console.log(this.alunosSeparados);
     this.alunosService.getAlunos().subscribe((response: Aluno[]) => {
       this.alunos = response;
       this.alunosSeparados = [];
+
       this.alunos.map( e => {
       const filter = this.alunosSeparados.filter( element => e.nome === element.nome);
         if (filter.length === 0) {
           this.alunosSeparados.push(e);
         }
       });
-        this.alunos = this.alunosSeparados;
+
+      // this.alunos = this.alunosSeparados;
+
+      /** REALIZANDO BUSCA TULIZANDO FOREACH */
+      // let achou = false;
+      // this.alunos.forEach(e=>{
+      //   this.alunosSeparados.forEach(element => {
+      //     if (e.nome === element.nome){
+      //       achou = true;
+      //     }
+      //   });
+
+      //   if (!achou) {
+      //     this.alunosSeparados.push(e);
+      //   } else {
+      //     achou = false;
+      //   }
+      // });
+
+      // this.alunos = this.alunosSeparados;
     });
+
+
   }
 
   buscar(event){
